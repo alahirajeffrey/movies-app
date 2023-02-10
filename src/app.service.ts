b8c1dad73cbd1bdc72b98c1ca7786c7e5d93526e
alahirajeffrey <alahirajeffrey@gmail.com>
2023-02-10 13:59:36 +0100

    added services

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,24 +1,81 @@
 import { Injectable } from "@nestjs/common";
 import * as qrcode from "qrcode";
 import { Response } from "express";
-
-const URL = "www.google.com";
+import { ConfigService } from "@nestjs/config";
+import { HttpService } from "@nestjs/axios/dist";
+import { lastValueFrom } from "rxjs";
+import { Cron, CronExpression } from "@nestjs/schedule/dist";
 
 @Injectable()
 export class AppService {
-  private readonly configServide;
-  getHello(): string {
-    return "Hello World!";
+  constructor(
+    private configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {}
+
+  /**
+   *
+   * @returns string
+   */
+  homepage(): string {
+    return "Qr Code Generator";
   }
 
-  generateQrcode(stream: Response) {
-    qrcode
-      .toDataURL(URL)
-      .then((url) => {
-        return qrcode.toFileStream(stream, URL);
-      })
-      .then((error) => {
-        return error;
-      });
+  /**
+   * generate a random number between the range of 1 to 1000
+   * @returns a number
+   */
+  generateRandomNumber(): Number {
+    return Math.floor(Math.random() * 1000);
+  }
+
+  /**
+   * get list of movies from external api every 10 seconds
+   * @returns result: Array
+   */
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async movieList() {
+    try {
+      const API_URL = this.configService.get<string>("API_URL");
+      const API_KEY = this.configService.get<string>("API_KEY");
+
+      // generate random number to use as page parameter in get request
+      const moviePage = this.generateRandomNumber();
+
+      // make get request to themoviedb.org api
+      const result = await lastValueFrom(
+        this.httpService.get(`${API_URL}?api_key=${API_KEY}&page=${moviePage}`),
+      );
+
+      // return result from get request
+      const completeResultList: Array<any> = result.data.results;
+      const partialResultList: Array<any> = completeResultList.slice(0, 10);
+      return partialResultList;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  /**
+   * generates qr code from the url containing the movie list
+   * @param stream: express.Response
+   */
+  generateQrcode(stream: Response): void {
+    try {
+      const BASE_URL = this.configService.get<string>("BASE_URL");
+      const URL = `${BASE_URL}/movie-list`;
+
+      // generate qr code with the url of the movie list route
+      qrcode
+        .toDataURL(URL)
+        .then((url) => {
+          return qrcode.toFileStream(stream, URL);
+        })
+        .then((error) => {
+          return error;
+        });
+    } catch (error: any) {
+      console.log(error.message);
+    }
   }
 }
