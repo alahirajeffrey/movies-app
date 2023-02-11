dacfabe590cfa50be1ecdf1ae74ff670c6d4dc29
alahirajeffrey <alahirajeffrey@gmail.com>
2023-02-11 15:24:47 +0100

    added routes to get movie by ud and title

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, Res } from "@nestjs/common";
+import { Body, Controller, Get, Param, Res } from "@nestjs/common";
 import { Response } from "express";
 import { AppService } from "./app.service";
+import { MovieService } from "./movie.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly movieService: MovieService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -19,5 +23,15 @@ export class AppController {
   @Get("movie-list")
   async generateMovieList() {
     return await this.appService.movieList();
+  }
+
+  @Get("movie/title/:title")
+  async getMovieByTitle(@Param("title") title: string) {
+    return await this.movieService.getMovieById(title);
+  }
+
+  @Get("movie/:id")
+  async getMovieById(@Param("id") id: string) {
+    return await this.movieService.getMovieById(id);
   }
 }
