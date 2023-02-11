0aeef5b36b2ab6baddff5eb71b6e19059b972535
alahirajeffrey <alahirajeffrey@gmail.com>
2023-02-11 15:25:09 +0100

    added movie and prisma service to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { ConfigModule } from "@nestjs/config";
 import { ScheduleModule } from "@nestjs/schedule";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { MovieService } from "./movie.service";
+import { PrismaService } from "./prisma.service";
 
 @Module({
   imports: [ConfigModule.forRoot(), HttpModule, ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MovieService, PrismaService],
 })
 export class AppModule {}
