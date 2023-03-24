af0752b9ab43009924cc8de75bc190e92c39e3f1
alahirajeffrey <alahirajeffrey@gmail.com>
2023-03-24 07:29:33 +0100

    updated imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { ScheduleModule } from "@nestjs/schedule";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { MovieService } from "./movie.service";
-import { PrismaService } from "./prisma.service";
+import { AppController } from "./controllers/app.controller";
+import { AppService } from "./services/app.service";
+import { MovieService } from "./services/movie.service";
+import { PrismaService } from "./services/prisma.service";
 
 @Module({
   imports: [ConfigModule.forRoot(), HttpModule, ScheduleModule.forRoot()],
